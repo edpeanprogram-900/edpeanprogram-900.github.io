8a3857eefe21eb60649393807aa9ef7b2bfeb20a
edpeanprogram-900 <edpeanprogram@gmail.com>
2026-02-26 10:29:51 +0100

    apuntes LLMM

--- 09_practica_JavaScript_01/Ejercicio_03/js/script.js
+++ 09_practica_JavaScript_01/Ejercicio_03/js/script.js
@@ -1,20 +1,22 @@
 
 // EJERCICIO 3: cifrado César
 
-function cifradoCesar(texto, clave) {
-    let resultado = "";
+// VERSIÓN indicada por Pascual -------------------------------------------------------------------------------------------------------------------------------------
 
-    for (let char of texto) {   // esto equivale al for-each de Java. -> char irá tomando como valor los diferentes caracteres del texto de forma sucesiva. 
-        if (char.match(/[A-Z]/i)) {    // compruebo que el caracter sea una letra del alfabeto -> las barras son para indicar que estoy introduciendo una expresión regular. La "i" es para indicarle que no diferencie entre mayúsculas y minúsculas. 
-            let codigo = char.toUpperCase().charCodeAt(0);  // convierto el código a mayúscula y luego obtengo el código Unicode de esa letra. 
-            let nueva = ((codigo - 65 + clave) % 26) + 65;  // al restarle 65 logro que el código unicode de la letra se transforme en un número del 0 a 25. Le sumo después el desplazamiento. Luego obtengo el resto de dividir entre 26. Luego le sumo los 65. De este modo obtengo el código Unicode de la letra CODIFICADA (desplazada).
-            resultado = resultado + String.fromCharCode(nueva);  // para poder concatenarlo lo convierto a String. 
-        }
-    }
-    return resultado;
+let alfabeto = "abcdefghijklmnñopqrstuvwxyz";
+let frase = "casa";
+let paso = 3;
+let fraseCifrada = "";
+
+for (let i=0; i<frase.length; i++) {
+    console.log(frase[i]);
+    let posicion = (alfabeto.indexOf(frase[i])+paso) % alfabeto.length;      // usamos el módulo para que nunca se salga del array (que no de el error de out of bouns).
+    let letraCifrada = alfabeto[posicion];
+    fraseCifrada = faseCifrada + letraCifrada;
+
 }
 
-console.log("Cifrado César:", cifradoCesar("HOLA", 3));
+console.log(fraseCifrada);
 
 
 
@@ -40,6 +42,30 @@ Haces lo contrario: en vez de mover hacia delante, mueves cada letra kk posicion
 
 
 */
+
+
+
+
+
+// Versión empleando .charCodeAt() -------------------------------------------------------------------------------------------------------------------------------------
+
+
+function cifradoCesar(texto, clave) {
+    let resultado = "";
+
+    for (let char of texto) {                                      // esto equivale al for-each de Java. -> char irá tomando como valor los diferentes caracteres del texto de forma sucesiva. 
+        if (char.match(/[A-Z]/i)) {                               // compruebo que el caracter sea una letra del alfabeto -> las barras son para indicar que estoy introduciendo una expresión regular. La "i" es para indicarle que no diferencie entre mayúsculas y minúsculas. 
+            let codigo = char.toUpperCase().charCodeAt(0);         // convierto el código a mayúscula y luego obtengo el código Unicode de esa letra. 
+            let nueva = ((codigo - 65 + clave) % 26) + 65;         // al restarle 65 logro que el código unicode de la letra se transforme en un número del 0 a 25. Le sumo después el desplazamiento. Luego obtengo el resto de dividir entre 26. Luego le sumo los 65. De este modo obtengo el código Unicode de la letra CODIFICADA (desplazada).
+            resultado = resultado + String.fromCharCode(nueva);    // para poder concatenarlo lo convierto a String. 
+        }
+    }
+    return resultado;
+}
+
+console.log("Cifrado César:", cifradoCesar("HOLA", 3));
+
+
 
 
 /*
