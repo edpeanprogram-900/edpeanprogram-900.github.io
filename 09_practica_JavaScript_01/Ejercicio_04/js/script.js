1b3752ff4322fa51c1c5686f33af020a91ca701a
edpeanprogram-900 <edpeanprogram@gmail.com>
2026-02-27 09:13:10 +0100

    progresos en la tarea de LLMM javascript

--- 09_practica_JavaScript_01/Ejercicio_04/js/script.js
+++ 09_practica_JavaScript_01/Ejercicio_04/js/script.js
@@ -5,6 +5,14 @@ function contrasenya(num) {
 
     password = "";
 
+    if(num<8){
+        return "La contraseña debe tener como mínimo 8 caracteres.";
+    }
+
+    if(num>50){
+        return "La contraseña no puede tener más de 50 caracteres.";
+    }
+
     // Cantidades
     numDeNumeros = Math.floor(Math.random() * 2) + 1; 
     numCaracEsp = Math.floor(Math.random() * 2) + 1; 
@@ -16,21 +24,45 @@ function contrasenya(num) {
     caracNums = "0123456789";
     caracEsp = "!@#$%^&*()_+|~`-={}[]:;'<>?,./";
 
+    // Le añado los números.
+    for(let i=0; i<numDeNumeros; i++){   // en la condición pongo un "<" y no un "<=" porque la "i" se inicializa en cero. 
+                // Math.floor(Math.random() * max) + 1; -> este es el código para generar números aleatorios. -> le quito el +1 para que el resultado pueda ser 0 (charAt(0)).
+        password = password + (caracNums.charAt(Math.floor(Math.random() * (caracNums.length-1))));
+    }
 
-    return ____________________
+    // Le añado los carácteres especiales.
+    for(let i=0; i<numCaracEsp; i++){
+        password = password + (caracEsp.charAt(Math.floor(Math.random() * (caracEsp.length-1))));
+    }
+    
+    // Le añado las mayúsculas. 
+    password = password + (caracMayus.charAt(Math.floor(Math.random() * (caracMayus.length-1))));
+
+
+    // Completo hasta la longitud deseada con minúsculas
+    for(let i=0; i<num; i++){   // en la condición había puesto password.length y por lo tanto había caído en un bucle infinito (y la consola ni siquiera cargaba en la web debido a ello).
+        password = password + (caracMinus.charAt(Math.floor(Math.random() * (caracMinus.length-1))));
+    }
+
+
+    // Desordeno el array -> este código nos lo da el propio enunciado.
+    password = password.split('').sort(() => Math.random() - 0.5).join('');
+
+
+    return password;
 }
 
 
-// Determina la longitud de la contraseña
-function numeroAleatorio(min, max) {
-    minLength = 8;
-    maxLength = 50;
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+console.log("Contraseña generada: ", contrasenya(8));
 
 
 
-console.log("Generando contraseña: ", contrasenya(8));
+/*
+
+// Funciona correctamente, me ha dado esta salida: Contraseña generada:  fkfu31F]fxgf
+
+
+*/
 
 
 
